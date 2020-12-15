3560b566ab17468764f444d8a087b89773b0c465
Hugo J <hjbello.wk@gmail.com>
2020-12-15 14:16:54 +0100

    adds id to wallet

--- models/walletKey.go
+++ models/walletKey.go
@@ -2,11 +2,12 @@ package models
 
 const (
 	WalletKeyOwnerTypeDevice = "DEVICE"
-	WalletKeyOwnerTypeUSER = "USER"
+	WalletKeyOwnerTypeUSER   = "USER"
 )
 
 type WalletKey struct {
 	AccessPassword string `bson:"accessPassword,omitempty" json:"accessPassword,omitempty"`
+	ID             string `bson:"id,omitempty" json:"id,omitempty"`
 	OwnerId        string `bson:"ownerId,omitempty" json:"ownerId,omitempty"`
 	OwnerType      string `bson:"ownerType,omitempty" json:"ownerType,omitempty"`
 	Type           string `bson:"type,omitempty" json:"type,omitempty"`
