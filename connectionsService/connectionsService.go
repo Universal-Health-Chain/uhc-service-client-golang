a8eac78d6ee8bdf5f2a805dd2cbe2cbe92509355
Hugo J <hjbello.wk@gmail.com>
2020-12-31 18:59:15 +0100

    adds external token actions in connections

--- connectionsService/connectionsService.go
+++ connectionsService/connectionsService.go
@@ -7,19 +7,24 @@ import (
 type ConnectionsService struct {
 	ConnectionUhcController ConnectionUhcController
 	MessageUhcController MessageUhcController
+	ExternalConectionTokenActionsController ExternalConectionTokenActionsController
 }
 
 func (connectionsService *ConnectionsService) Initialize(backendUrl string) {
 	connectionsService.ConnectionUhcController = ConnectionUhcController{models.Service{BackendUrl: backendUrl}}
 	connectionsService.MessageUhcController = MessageUhcController{models.Service{BackendUrl: backendUrl}}
+	connectionsService.ExternalConectionTokenActionsController = ExternalConectionTokenActionsController{models.Service{BackendUrl: backendUrl}}
 }
 
 func (connectionsService *ConnectionsService) SetToken(token string) {
 	connectionsService.ConnectionUhcController.Token = token
 	connectionsService.MessageUhcController.Token = token
+	connectionsService.ExternalConectionTokenActionsController.Token = token
 }
 
 func (connectionsService *ConnectionsService) SetServiceToken(token string) {
 	connectionsService.ConnectionUhcController.ServiceToken = token
 	connectionsService.MessageUhcController.ServiceToken = token
+	connectionsService.ExternalConectionTokenActionsController.ServiceToken = token
+
 }
