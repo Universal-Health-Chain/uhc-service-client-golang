4d62cda1138e841dcec6b61bbd8e878c94e1cffb
Hugo J <hjbello.wk@gmail.com>
2021-01-01 17:40:40 +0100

    adds external message creation request

---
+++ models/externalBase64MessageCreationRequest.go
@@ -0,0 +1,8 @@
+package models
+
+type ExternalBase64MessageCreationRequest struct {
+	ExternalMessageDirectives ExternalMessageDirectives `json:"externalDirectives" bson:"externalDirectives"`
+	Base64Message             string                    `json:"base64Message,omitempty" bson:"base64Message,omitempty"`
+	Type                      string                    `json:"type,omitempty" bson:"type,omitempty"`
+	FileType                  *string                   `json:"FileType,omitempty" bson:"FileType,omitempty"`
+}
---
+++ models/externalFhirMessageCreationRequest.go
@@ -0,0 +1,16 @@
+package models
+
+import fhir4 "github.com/Universal-Health-Chain/golang-fhir-models-uhc/fhir-models/fhir"
+
+
+type ExternalFhirMessageCreationRequest struct {
+	ExternalMessageDirectives ExternalMessageDirectives `json:"externalDirectives" bson:"externalDirectives"`
+	FhirMessage        fhir4.Bundle              `json:"fhirMessage,omitempty" bson:"fhirMessage,omitempty"`
+}
+
+type ExternalMessageDirectives struct {
+	ToEmail               string  `json:"toEmail,omitempty" bson:"toEmail,omitempty"`
+	FromOrganizationEmail *string `json:"fromOrganizationEmail,omitempty" bson:"fromOrganizationEmail,omitempty"`
+	ToUserId              *string `json:"toUserId,omitempty" bson:"toUserId,omitempty"`
+}
+
