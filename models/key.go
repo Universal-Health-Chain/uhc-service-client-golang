b46fca983512e5c145b191f33f3a9c6b4d9ea324
Hugo J <hjbello.wk@gmail.com>
2021-01-11 10:55:15 +0100

    adds new field in key creation

--- models/key.go
+++ models/key.go
@@ -7,18 +7,18 @@ import (
 
 const (
 	// for 'Usage'
-	EncryptionKeyTypeEncryption = "ENCRYPTION"	// Please avoid this name and use KeyUsageEncryption
-	EncryptionKeyTypeSigning    = "SIGN"		// Please avoid this name and use KeyUsageSigning
-	KeyUsageEncryption 	= "ENCRYPTION"			// Use this one instead of EncryptionKeyTypeEncryption
-	KeyUsageSigning    	= "SIGN"				// Use this one instead of EncryptionKeyTypeSigning
+	EncryptionKeyTypeEncryption = "ENCRYPTION" // Please avoid this name and use KeyUsageEncryption
+	EncryptionKeyTypeSigning    = "SIGN"       // Please avoid this name and use KeyUsageSigning
+	KeyUsageEncryption          = "ENCRYPTION" // Use this one instead of EncryptionKeyTypeEncryption
+	KeyUsageSigning             = "SIGN"       // Use this one instead of EncryptionKeyTypeSigning
 	// for 'Type'
-	X25519KeyType 			= "X25519KeyAgreementKey2019"
-	Ed25519KeyType 			= "Ed25519VerificationKey2018"
+	X25519KeyType  = "X25519KeyAgreementKey2019"
+	Ed25519KeyType = "Ed25519VerificationKey2018"
 )
 
 type Key struct {
 	ID               string     `bson:"id,omitempty" json:"id,omitempty"`
-	WalletKeyId      string     `bson:"walletKeyId,omitempty" json:"walletKeyId,omitempty"`	// Parent Wallet ID
+	WalletKeyId      string     `bson:"walletKeyId,omitempty" json:"walletKeyId,omitempty"` // Parent Wallet ID
 	Tag              string     `bson:"tag,omitempty" json:"tag,omitempty"`
 	Purposes         []string   `bson:"capability,omitempty" json:"capability,omitempty"` // TODO: Change to []string
 	Type             string     `bson:"type,omitempty" json:"type,omitempty"`
@@ -27,8 +27,8 @@ type Key struct {
 	PrivateKeyBase64 string     `bson:"privateKeyBase64,omitempty" json:"privateKeyBase64,omitempty"`
 	CreatedAt        *time.Time `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
 	UpdatedAt        *time.Time `bson:"updatedAt,omitempty" json:"updatedAt,omitempty"`
-	ControllerDID    string     `bson:"controllerDid,omitempty" json:"controllerDid,omitempty"`	// "did:v1:uuid:" + uhcUserId
-	PublicKeyDID     string     `bson:"publicKeyDid,omitempty" json:"publicKeyDid,omitempty"` 	// "did:v1:uuid:" + uhcUserId + "#" + Key.ID
+	ControllerDID    string     `bson:"controllerDid,omitempty" json:"controllerDid,omitempty"` // "did:v1:uuid:" + uhcUserId
+	PublicKeyDID     string     `bson:"publicKeyDid,omitempty" json:"publicKeyDid,omitempty"`   // "did:v1:uuid:" + uhcUserId + "#" + Key.ID
 	Expires          *time.Time `bson:"expires,omitempty" json:"expires,omitempty"`
 	Revoked          *time.Time `bson:"revoked,omitempty" json:"revoked,omitempty"`
 }
@@ -41,12 +41,14 @@ type KeyRetrievalRequest struct {
 type KeyCreationRequest struct {
 	AccessPassword string `bson:"accessPassword,omitempty" json:"accessPassword,omitempty"`
 	Tag            string `bson:"tag,omitempty" json:"tag,omitempty"`
+	Usage          string `bson:"usage,omitempty" json:"usage,omitempty"`
 }
 
 type KeyCreationOrganizationRequest struct {
-	OrganizationId string `bson:"organizationId,omitempty" json:"organizationId,omitempty"`
+	OrganizationId string  `bson:"organizationId,omitempty" json:"organizationId,omitempty"`
 	AccessPassword *string `bson:"accessPassword,omitempty" json:"accessPassword,omitempty"`
-	Tag            string `bson:"tag,omitempty" json:"tag,omitempty"`
+	Tag            string  `bson:"tag,omitempty" json:"tag,omitempty"`
+	Usage          string `bson:"usage,omitempty" json:"usage,omitempty"`
 }
 
 type KeyResponse struct {
@@ -74,12 +76,12 @@ type PublicInfoFromKeyResponse struct {
 
 // Standard expected properties: id (did#id), type, controller, publicKeyBase58 (https://w3c-ccg.github.io/security-vocab/)
 type W3CPublicKeyExport struct { // controller is the blockchain's DID of the entity e.g. "did:v1:uuid:804c6ac3-ce3b-46ce-b134-17175d5bee74"
-	IdWithDid       string     	`bson:"id,omitempty" json:"id,omitempty"`							// Identifier of this public key in the owner's blockchain DID document
-	Controller		string		`bson:"controller,omitempty" json:"controller,omitempty"`			// didID already included in 'id'
-	Type            string     	`bson:"type,omitempty" json:"type,omitempty"`						// "Ed25519VerificationKey2018""
-	Expires			*time.Time	`bson:"expires,omitempty" json:"expires,omitempty"`
-	Revoked			*time.Time	`bson:"revoked,omitempty" json:"revoked,omitempty"`
-	PublicKeyBase64 string     	`bson:"publicKeyBase64,omitempty" json:"publicKeyBase64,omitempty"`
-	Context      	[]string	`bson:"@context,omitempty" json:"@context,omitempty"`				// "@context": ["https://w3id.org/security/v2"]
-	PublicKeyBase58 string     	`bson:"publicKeyBase58,omitempty" json:"publicKeyBase58,omitempty"`	// standard
+	IdWithDid       string     `bson:"id,omitempty" json:"id,omitempty"`                 // Identifier of this public key in the owner's blockchain DID document
+	Controller      string     `bson:"controller,omitempty" json:"controller,omitempty"` // didID already included in 'id'
+	Type            string     `bson:"type,omitempty" json:"type,omitempty"`             // "Ed25519VerificationKey2018""
+	Expires         *time.Time `bson:"expires,omitempty" json:"expires,omitempty"`
+	Revoked         *time.Time `bson:"revoked,omitempty" json:"revoked,omitempty"`
+	PublicKeyBase64 string     `bson:"publicKeyBase64,omitempty" json:"publicKeyBase64,omitempty"`
+	Context         []string   `bson:"@context,omitempty" json:"@context,omitempty"`               // "@context": ["https://w3id.org/security/v2"]
+	PublicKeyBase58 string     `bson:"publicKeyBase58,omitempty" json:"publicKeyBase58,omitempty"` // standard
 }
