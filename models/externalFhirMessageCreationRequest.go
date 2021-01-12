3111e228203b6a1f67698a25b5699062ff9f67bd
Hugo J <hjbello.wk@gmail.com>
2021-01-12 13:58:21 +0100

    adds new fields to identify sender of external messages

--- models/externalFhirMessageCreationRequest.go
+++ models/externalFhirMessageCreationRequest.go
@@ -15,7 +15,7 @@ type ExternalMessageDirectives struct {
 }
 
 type ExternalSenderUserDetails struct {
-	Email               string  `json:"toEmail,omitempty" bson:"toEmail,omitempty"`
+	Email               string  `json:"email,omitempty" bson:"email,omitempty"`
 	FirstName           string  `json:"firstName,omitempty" bson:"firstName,omitempty"`
 	LastName            string  `json:"lastName,omitempty" bson:"lastName,omitempty"`
 	TitleInOrganization *string `json:"titleInOrganization,omitempty" bson:"titleInOrganization,omitempty"`
