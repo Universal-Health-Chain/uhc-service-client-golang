a710df5bb0f299d3dbd9543b741de380994664dd
C. Hawks <claudia@conectate-soluciones.com>
2021-01-15 17:10:38 +0100

    adds aditional field in the register request

--- models/user.go
+++ models/user.go
@@ -48,6 +48,7 @@ type UserRegisterRequest struct {
 	FirstName		string `bson:"firstName,omitempty" json:"firstName,omitempty"`
 	LastName		string `bson:"lastName,omitempty" json:"lastName,omitempty"`
 	Email			string `bson:"email,omitempty" json:"email,omitempty"`
+	AppId 			*string `bson:"appId,omitempty" json:"appId,omitempty"`
 	LoginPassword	string `bson:"loginPassword,omitempty" json:"loginPassword,omitempty"`
 	WalletPassword	string `bson:"walletPassword,omitempty" json:"walletPassword,omitempty"`
 }
