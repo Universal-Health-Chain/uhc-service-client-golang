5ba90a5b1c2f3dc0f1c18164bb5392312071a677
Hugo J <hjbello.wk@gmail.com>
2021-01-18 18:58:00 +0100

    adds ehr request

--- models/encryptedEhr.go
+++ models/encryptedEhr.go
@@ -1,18 +1,27 @@
 package models
 
+import "time"
+
 type EncryptedEhr struct {
-	ID                     string   `bson:"id,omitempty" json:"id,omitempty"`
-	Type                   string   `bson:"type,omitempty" json:"type,omitempty"`
-	UhcUserId              string   `json:"uhcUserId,omitempty" bson:"uhcUserId,omitempty"`
-	KeyId                  string   `json:"keyId,omitempty" bson:"keyId,omitempty"`
-	EncryptedPayloadBase64 string   `bson:"encryptedPayloadBase64,omitempty" json:"encryptedPayloadBase64,omitempty"`
-	Codes                  []string `bson:"codes,omitempty" json:"codes,omitempty"`
-	UhcCodeTags            []string `bson:"uhcCodeTags,omitempty" json:"uhcCodeTags,omitempty"`
+	ID                     string     `bson:"id,omitempty" json:"id,omitempty"`
+	Type                   string     `bson:"type,omitempty" json:"type,omitempty"`
+	UhcUserId              string     `json:"uhcUserId,omitempty" bson:"uhcUserId,omitempty"`
+	EncryptionKeyId        string     `json:"encryptionKeyId,omitempty" bson:"encryptionKeyId,omitempty"`
+	EncryptedPayloadBase64 string     `bson:"encryptedPayloadBase64,omitempty" json:"encryptedPayloadBase64,omitempty"`
+	Codes                  []string   `bson:"codes,omitempty" json:"codes,omitempty"`
+	UhcCodeTags            []string   `bson:"uhcCodeTags,omitempty" json:"uhcCodeTags,omitempty"`
+	CreatedAt              *time.Time `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
+	UpdatedAt              *time.Time `bson:"updatedAt,omitempty" json:"updatedAt,omitempty"`
+}
+
+type EncryptedEhrCreationRequest struct {
+	BiographyUHC   BiographyUHC `bson:"biographyUHC,omitempty" json:"biographyUHC,omitempty"`
+	AccessPassword string       `bson:"accessPassword,omitempty" json:"accessPassword,omitempty"`
 }
 
 type EncryptedEhrResponse struct {
-	Code    int               `bson:"code,omitempty" json:"code,omitempty"`
-	Count   int64             `bson:"count,omitempty" json:"count,omitempty"`
-	Message string            `bson:"message,omitempty" json:"message,omitempty"`
+	Code    int            `bson:"code,omitempty" json:"code,omitempty"`
+	Count   int64          `bson:"count,omitempty" json:"count,omitempty"`
+	Message string         `bson:"message,omitempty" json:"message,omitempty"`
 	Data    []EncryptedEhr `bson:"data,omitempty" json:"data,omitempty"`
 }
