85c16035c454423c213a579183f29f67270ce235
Hugo J <hjbello.wk@gmail.com>
2021-01-19 11:33:54 +0100

    encrypted ehr decription response models

--- models/encryptedEhr.go
+++ models/encryptedEhr.go
@@ -19,9 +19,22 @@ type EncryptedEhrCreationRequest struct {
 	AccessPassword string       `bson:"accessPassword,omitempty" json:"accessPassword,omitempty"`
 }
 
+type EncryptedEhrDecryptionRequest struct {
+	EncryptedEhrId *string   `bson:"encryptedEhrId,omitempty" json:"encryptedEhrId,omitempty"`
+	UhcCodeTags    *[]string `bson:"uhcCodeTags,omitempty" json:"uhcCodeTags,omitempty"`
+	AccessPassword string    `bson:"accessPassword,omitempty" json:"accessPassword,omitempty"`
+}
+
 type EncryptedEhrResponse struct {
 	Code    int            `bson:"code,omitempty" json:"code,omitempty"`
 	Count   int64          `bson:"count,omitempty" json:"count,omitempty"`
 	Message string         `bson:"message,omitempty" json:"message,omitempty"`
 	Data    []EncryptedEhr `bson:"data,omitempty" json:"data,omitempty"`
+}
+
+type EncryptedEhrDecryptionResponse struct {
+	Code    int            `bson:"code,omitempty" json:"code,omitempty"`
+	Count   int64          `bson:"count,omitempty" json:"count,omitempty"`
+	Message string         `bson:"message,omitempty" json:"message,omitempty"`
+	Data    []BiographyUHC `bson:"data,omitempty" json:"data,omitempty"`
 }
